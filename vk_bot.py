7081610637c4d74ac97287fa9a805c26c42d7b68
ArtemZinukov <zinykov_a@mail.ru>
2024-09-12 19:19:17 +0300

    echobot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -1,23 +1,24 @@
+import random
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
 
 
 def listen_for_messages(vk_session):
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            handle_new_message(event)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
 
 
-def handle_new_message(event):
-    print('Новое сообщение:')
-    if event.to_me:
-        print('Для меня от: ', event.user_id)
-    else:
-        print('От меня для: ', event.user_id)
-    print('Текст:', event.text)
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
 
 
 def main():
@@ -29,5 +30,14 @@ def main():
     listen_for_messages(vk_session)
 
 
+
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
